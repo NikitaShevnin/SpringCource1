78fd1d5ee302df16a02b674a9d6b0ae3186e9212
ВТБ <nikita.shevnin2016@yandex.ru>
2024-05-10 12:55:42 +0300

    Solution HW for 5 seminar

---
+++ src/main/java/sem5/hw/Solution_HW_for_sem5_SpringDataJPA/src/test/java/ru/gb/edu/solution/SolutionHwForSem5SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.edu.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionHwForSem5SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
